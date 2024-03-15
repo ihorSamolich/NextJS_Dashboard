22e8450cfeda4870ec7357cf8663ef3881460a3e
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-03-15 13:01:16 +0200

    Add auth

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import React from 'react';
+import { Metadata } from 'next';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
